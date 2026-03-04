d9244ce1600ee985603243f4ee1e1ed74f72bb07
Kavindu49 <kavindupinsara2003@gmail.com>
2026-03-04 15:53:43 +0530

    Add new logic in driver_moniter_screen

--- driveguard/lib/screens/bottom_navbar_pages/driver_monitor/driver_monitor_screen.dart
+++ driveguard/lib/screens/bottom_navbar_pages/driver_monitor/driver_monitor_screen.dart
@@ -1,8 +1,8 @@
 import 'package:blinker/blinker.dart';
-import 'package:driveguard/provider/driver_live_monitor/driver_live_monitor.dart';
-import 'package:driveguard/provider/esp_device_provider/esp_device_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:driveguard/provider/driver_live_monitor/driver_live_monitor.dart';
+import 'package:driveguard/provider/esp_device_provider/esp_device_provider.dart';
 
 class DriverMonitorScreen extends StatefulWidget {
   const DriverMonitorScreen({super.key});
@@ -12,96 +12,83 @@ class DriverMonitorScreen extends StatefulWidget {
 }
 
 class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
+
   @override
   Widget build(BuildContext context) {
-    return Consumer2<DriverLiveMonitor, EspDeviceProvider>(
-      builder:
-          (
-            BuildContext context,
-            DriverLiveMonitor driver_monitor,
-            EspDeviceProvider esp_connector,
-            Widget? child,
-          ) {
-            return Scaffold(
-              appBar: AppBar(
-                surfaceTintColor: Colors.black,
-                title: Text(
-                  'Driver Live Monitor',
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white70,
-                  ),
-                ),
-                actions: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.bluetooth,
-                        color: esp_connector.isDriverMonitorConnecteed
-                            ? Colors.green
-                            : Colors.grey,
-                        size: 30,
+    return Consumer2<DriverLiveMonitor,EspDeviceProvider>(
+      builder: (BuildContext context, DriverLiveMonitor driver_monitor,EspDeviceProvider esp_connector, Widget? child) {
+        return Scaffold(
+          appBar: AppBar(
+            surfaceTintColor: Colors.black,
+            title:Text(
+              'Driver Live Monitor',
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.w600,
+                color: Colors.white70,
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: IconButton(icon: Icon(Icons.bluetooth,color: esp_connector.isDriverMonitorConnecteed?Colors.green:Colors.grey, size: 30), onPressed: () {
+                  Provider.of<EspDeviceProvider>(context,listen: false).startdrivermonitorScan();
+                },),
+              )
+            ],
+            centerTitle: true,
+            leading: driver_monitor.isDriverAlert?Blinker.fade(
+                startColor: Colors.grey,
+                endColor: Colors.red,
+                duration: Duration(milliseconds: 500),
+                child: const Icon(Icons.warning_rounded, color: Colors.grey, size: 30)):Icon(Icons.warning_rounded, color: Colors.grey, size: 30),
+          ),
+          body: Column(
+            children: [
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 5,
+                    children: [
+                      _buildMonitorCard(
+                        title: 'Blood Oxygen (SpO2)',
+                        value: driver_monitor.bloodOxygenLevel.toStringAsFixed(0),
+                        unit: '%',
+                        icon: Icons.bloodtype,
+                        color: Colors.green,
                       ),
-                      onPressed: () {
-                        Provider.of<EspDeviceProvider>(
-                          context,
-                          listen: false,
-                        ).startdrivermonitorScan();
-                      },
-                    ),
+                      _buildMonitorCard(
+                        title: 'Cabin Temperature',
+                        value: driver_monitor.temperature.toStringAsFixed(0),
+                        unit: '°C',
+                        icon: Icons.thermostat,
+                        color: Colors.orangeAccent,
+                      ),
+                      _buildMonitorCard(
+                        title: 'Blood Preasure',
+                        value: driver_monitor.bloodPressure.toStringAsFixed(0),
+                        unit: 'BPM',
+                        icon: Icons.bloodtype_outlined,
+                        color: Colors.redAccent,
+                      ),
+                      _buildMonitorCard(
+                        title: 'Cabin Noise',
+                        value: driver_monitor.cabinNoiseLevel.toStringAsFixed(0),
+                        unit: 'dB',
+                        icon: Icons.record_voice_over_outlined,
+                        color: Colors.blueAccent,
+                      ),
+                    ],
                   ),
-                ],
-                centerTitle: true,
-                leading: driver_monitor.isDriverAlert
-                    ? Blinker.fade(
-                        startColor: Colors.grey,
-                        endColor: Colors.red,
-                        duration: Duration(milliseconds: 500),
-                        child: const Icon(
-                          Icons.warning_rounded,
-                          color: Colors.grey,
-                          size: 30,
-                        ),
-                      )
-                    : Icon(Icons.warning_rounded, color: Colors.grey, size: 30),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ListView(
-                  children: [
-                    _buildMonitorCard(
-                      title: 'Blood Oxygen (SpO2)',
-                      value: driver_monitor.bloodOxygenLevel.toStringAsFixed(0),
-                      unit: '%',
-                      icon: Icons.bloodtype,
-                      color: Colors.green,
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    _buildMonitorCard(
-                      title: 'Cabin Temperature',
-                      value: driver_monitor.temperature.toStringAsFixed(0),
-                      unit: '°C',
-                      icon: Icons.thermostat,
-                      color: Colors.orangeAccent,
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    _buildMonitorCard(
-                      title: 'Blood Preasure',
-                      value: driver_monitor.bloodPreasure.toStringAsFixed(0),
-                      unit: 'BPM',
-                      icon: Icons.bloodtype_outlined,
-                      color: Colors.redAccent,
-                    ),
-                  ],
                 ),
               ),
-            );
-          },
+              !driver_monitor.isDriverAlert?buildDriverStatusCard(driver_monitor):SizedBox()
+            ],
+          ),
+        );
+      },
     );
   }
 
@@ -126,39 +113,76 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
             colors: [color.withOpacity(0.1), Colors.white],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, size: 50, color: color),
-            const SizedBox(height: 10),
-            Text(
-              title,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              textBaseline: TextBaseline.alphabetic,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
+              Icon(icon, size: 45, color: color),
+              const SizedBox(height: 10),
+              Text(
+                title,
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    value,
+                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    unit,
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildDriverStatusCard(DriverLiveMonitor driver_monitor) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.orange.withOpacity(0.4),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.warning_amber_rounded, size: 50, color: Colors.white),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  value,
+                  "${driver_monitor.alertMessage}",
                   style: const TextStyle(
-                    fontSize: 48,
+                    fontSize: 18,
                     fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  unit,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
